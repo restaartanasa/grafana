86c511bd23e70bc4bbae1a3abe368d9f435f2209
Hugo Häggmark <hugo.haggmark@gmail.com>
2025-12-18 10:25:38 +0100

    GrafanaBootData: decouple `config.apps` from boot data IV

---
+++ packages/grafana-runtime/src/services/pluginMeta/types.ts
@@ -0,0 +1,10 @@
+import { AppPluginConfig } from '@grafana/data';
+
+import { Meta } from '../../../../../apps/plugins/plugin/src/generated/meta/v0alpha1/meta_object_gen';
+
+export type AppPluginMetas = Record<string, AppPluginConfig>;
+
+export type AppPluginMetasMapper<T> = (response: T) => AppPluginMetas;
+export interface PluginMetasResponse {
+  items: Meta[];
+}
